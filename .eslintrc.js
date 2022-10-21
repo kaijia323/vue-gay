a24c14fbc2ecd604f323dd56d7d56c534201df07
kaijia <kaijia@kaijia.com>
2022-10-21 15:14:51 +0800

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    // 'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  rules: {
+    'vue/no-unused-vars': 'error',
+    'vue/multi-word-component-names': 'off',
+  },
+}
---
+++ packages/components/gayButton/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const a = '1'
+</script>
+
+<template>
+  <div a="1" b="2" c="3" d="4"></div>
+</template>
